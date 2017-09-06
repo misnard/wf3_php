ad151c2624415a8c47f8cb010c1eb46f131775cb
misnard <maher.isnard@gmail.com>
2017-09-06 16:41:41 +0200

    Deuxieme commit pour ajouter les pages post et index

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include('include/display_art.php'); ?>
 <!DOCTYPE HTML>
 <!--
 	Future Imperfect by HTML5 UP
@@ -97,36 +98,7 @@
 
 		<!-- Main -->
 		<div id="main">
-
-			<!-- Post -->
-			<article class="post">
-				<header>
-					<div class="title">
-						<h2><a href="#">Magna sed adipiscing</a></h2>
-						<p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
-					</div>
-					<div class="meta">
-						<time class="published" datetime="2015-11-01">November 1, 2015</time>
-						<a href="#" class="author"><span class="name">Jane Doe</span><img src="images/avatar.jpg" alt="" /></a>
-					</div>
-				</header>
-				<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
-				<p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
-				<footer>
-					<ul class="actions">
-						<li><a href="#" class="button big">Continue Reading</a></li>
-					</ul>
-					<ul class="stats">
-						<li><a href="#">General</a></li>
-						<li><a href="#" class="icon fa-heart">28</a></li>
-						<li><a href="#" class="icon fa-comment">128</a></li>
-					</ul>
-				</footer>
-			</article>
-
-
-
-
+			<?php display_articles(); ?>
 
 			<!-- Pagination -->
 			<ul class="actions pagination">

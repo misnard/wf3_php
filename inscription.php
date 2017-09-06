1ef160a67cd10ce9ff74ab3186d69880c75d223e
Sly <sylvain.rey.pro@gmail.com>
2017-09-06 16:41:18 +0200

    Page inscription terminee

--- inscription.php
+++ inscription.php
@@ -1,29 +1,56 @@
 <?php
-if(!empty($_POST))
+if(!empty($_POST)) // Si les données du formulaire ne sont pas vides
 {
 	require('isRecaptchaValid.php');
 
 	if( // Si les champs ont été envoyés par le client et qu'ils ne sont pas vide
     isset($_POST['email']) && !empty($_POST['email']) 
-    && $_POST['pseudo'] && !empty($_POST['pseudo']) 
-    && $_POST['password'] && !empty($_POST['password']))
+    && $_POST['uname'] && !empty($_POST['uname']) 
+    && $_POST['password'] && !empty($_POST['password'])
+    && $_POST['password-confirm'] && !empty($_POST['password-confirm']))
 	{
         // Si le captcha est invalide afficher un message d'erreur en dessous du formulaire
         if(!isRecaptchaValid($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']))
         {
             $errors[] = 'Captcha Invalide!';
         }
+        
+        if($_POST['password'] != $_POST['password-confirm'])
+        {
+            $errors[] = 'Les mot de passe ne correspondent pas.';
+        }
+        if(!isset($errors)) // Connexion à la base de donnée avec un prepare et sécurisation des données contre les failles XSS
+        {
+            try
+            {
+                $bdd = new PDO('mysql:host=localhost;dbname=tp_php;charset=utf8','root','');
+            } 
+            catch(Exception $e)
+            {
+                die('Erreur: ' . $e->getMessage());
+            }
+                $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $response = $bdd->prepare("INSERT INTO user(uname, email, password, admin) VALUES(?,?,?,?)");
+                $response->execute(array(
+                    $_POST['uname'],
+                    $_POST['email'],
+                    password_hash($_POST['password'], PASSWORD_BCRYPT),
+                    $admin = 0
+                ));
+            
+            $success[] = 'Merci de vous êtes inscrit.';
+        }
         else
         {
-            
+	       $errors[] = 'Il y a eu un problème durant l\'envoi du formulaire.';
         }
+        
     }
-	else
-	{
-		$errors[] = 'Remplissez tous les champs.';
-	}
+    else
+    {
+        $errors[] = "Merci de remplir tous les champs.";
+    }
 }
-
 ?>
 <!DOCTYPE HTML>
 <!--
@@ -77,13 +104,13 @@ if(!empty($_POST))
 							</ul>
 						</nav>
 					</header>
-
+       
                 <form method="post" action="#">
                                         <div class="row uniform">
                                             <div class="6u">                                               <input type="email" name="email" placeholder="Email" />
                                             </div>
                                             <div class="6u">
-                                               <input type="text" name="pseudo" placeholder="Pseudo" />
+                                               <input type="text" name="uname" placeholder="Pseudo" />
 
                                             </div>
                                             <div class="6u">
@@ -95,8 +122,8 @@ if(!empty($_POST))
                                             
                                             <!-- Captcha -->
                                             <div class="6u">
+                                               <label for "g-recaptcha">Merci de remplir le captcha</label>
                                                 <div class="g-recaptcha" data-sitekey="6Lc9SS4UAAAAAFUTghLqVMQluTzujXUlUP821k2g"></div>
-
                                             </div>
                                 
                                                 <ul class="actions">
@@ -105,19 +132,20 @@ if(!empty($_POST))
                                                 </ul>
                                             </div>
                                     </form>
+                                    
 <?php
 if(isset($errors))
 {
 	foreach($errors as $error)
 	{
-		echo $error.'<br>';
+		echo '<p style=color:red;>'.$error.'</p><br>';
 	}
 }
         
 if(isset($success))
 {
     foreach($success as $ok)
-	echo $ok.'<br>';
+	echo '<p style=color:green;>'.$ok.'</p><br>';
 }
 ?>
 

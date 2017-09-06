229197625b80c69a46b0090131a0a87ea4089f68
Pierre Simon <pierre.simon01480@yahoo.fr>
2017-09-06 14:37:09 +0200

    modif2 index.html

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE HTML>
+<!--
+	Future Imperfect by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+	<title>Je suis une bobo</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+</head>
+<body>
+
+	<!-- Wrapper -->
+	<div id="wrapper">
+
+		<!-- Header -->
+		<header id="header">
+			<h1><a href="#">BLOG D'UN BOBO</a></h1>
+			<nav class="links">
+				<ul>
+					<li><a href="#">Connexion</a></li>
+					<li><a href="#">Inscription</a></li>
+					<li><a href="#">Profil</a></li>
+					<li><a href="#">Administration</a></li>
+					<li><a href="#">Déconnexion</a></li>
+				</ul>
+			</nav>
+			<nav class="main">
+				<ul>
+					<li class="search">
+						<a class="fa-search" href="#search">Search</a>
+						<form id="search" method="get" action="#">
+							<input type="text" name="query" placeholder="Search" />
+						</form>
+					</li>
+					<li class="menu">
+						<a class="fa-bars" href="#menu">Menu</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+
+		<!-- Menu -->
+		<section id="menu">
+
+			<!-- Search -->
+			<section>
+				<form class="search" method="get" action="#">
+					<input type="text" name="query" placeholder="Search" />
+				</form>
+			</section>
+
+			<!-- Links -->
+			<section>
+				<ul class="links">
+					<li>
+						<a href="#">
+							<h3>Lorem ipsum</h3>
+							<p>Feugiat tempus veroeros dolor</p>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<h3>Dolor sit amet</h3>
+							<p>Sed vitae justo condimentum</p>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<h3>Feugiat veroeros</h3>
+							<p>Phasellus sed ultricies mi congue</p>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<h3>Etiam sed consequat</h3>
+							<p>Porta lectus amet ultricies</p>
+						</a>
+					</li>
+				</ul>
+			</section>
+
+			<!-- Actions -->
+			<section>
+				<ul class="actions vertical">
+					<li><a href="#" class="button big fit">Log In</a></li>
+				</ul>
+			</section>
+
+		</section>
+
+		<!-- Main -->
+		<div id="main">
+
+			<!-- Post -->
+			<article class="post">
+				<header>
+					<div class="title">
+						<h2><a href="#">Magna sed adipiscing</a></h2>
+						<p>Lorem ipsum dolor amet nullam consequat etiam feugiat</p>
+					</div>
+					<div class="meta">
+						<time class="published" datetime="2015-11-01">November 1, 2015</time>
+						<a href="#" class="author"><span class="name">Jane Doe</span><img src="images/avatar.jpg" alt="" /></a>
+					</div>
+				</header>
+				<a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+				<p>Mauris neque quam, fermentum ut nisl vitae, convallis maximus nisl. Sed mattis nunc id lorem euismod placerat. Vivamus porttitor magna enim, ac accumsan tortor cursus at. Phasellus sed ultricies mi non congue ullam corper. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+				<footer>
+					<ul class="actions">
+						<li><a href="#" class="button big">Continue Reading</a></li>
+					</ul>
+					<ul class="stats">
+						<li><a href="#">General</a></li>
+						<li><a href="#" class="icon fa-heart">28</a></li>
+						<li><a href="#" class="icon fa-comment">128</a></li>
+					</ul>
+				</footer>
+			</article>
+
+
+
+
+
+			<!-- Pagination -->
+			<ul class="actions pagination">
+				<li><a href="" class="disabled button big previous">Previous Page</a></li>
+				<li><a href="#" class="button big next">Next Page</a></li>
+			</ul>
+
+		</div>
+
+		<!-- Sidebar -->
+		<section id="sidebar">
+
+			<!-- Intro -->
+			<section id="intro">
+				<a href="#" class="logo"><img src="images/logo.jpg" alt="" /></a>
+				<header>
+					<h2>Les bobos de Paris</h2>
+					<p>Le blog des bobos de Paris intra-muros</a></p>
+				</header>
+			</section>
+
+			<!-- Mini Posts -->
+			<section>
+				<div class="mini-posts">
+
+					<!-- Mini Post -->
+					<article class="mini-post">
+						<header>
+							<h3><a href="#">Vitae sed condimentum</a></h3>
+							<time class="published" datetime="2015-10-20">October 20, 2015</time>
+							<a href="#" class="author"><img src="images/avatar.jpg" alt="" /></a>
+						</header>
+						<a href="#" class="image"><img src="images/pic04.jpg" alt="" /></a>
+					</article>
+
+
+
+				</div>
+			</section>
+
+			<!-- Posts List -->
+			<section>
+				<ul class="posts">
+					<li>
+						<article>
+							<header>
+								<h3><a href="#">Lorem ipsum fermentum ut nisl vitae</a></h3>
+								<time class="published" datetime="2015-10-20">October 20, 2015</time>
+							</header>
+							<a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>
+						</article>
+					</li>
+
+				</ul>
+			</section>
+
+			<!-- About -->
+			<section class="blurb">
+			<h2>A prpopos</h2>
+				<p>Voia ma vie, mon oeuvre, et bla bla bla bla !!</p>
+				<ul class="actions">
+					<li><a href="#" class="button">Learn More</a></li>
+				</ul>
+			</section>
+
+			<!-- Footer -->
+
+		</section>
+
+	</div>
+
+	<!-- Scripts -->
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/skel.min.js"></script>
+	<script src="assets/js/util.js"></script>
+	<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+	<script src="assets/js/main.js"></script>
+
+</body>
+</html>

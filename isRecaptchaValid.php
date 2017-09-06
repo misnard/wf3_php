eb1307ab46a3fd93e1db1b240f8383165c993512
Sly <sylvain.rey.pro@gmail.com>
2017-09-06 15:15:30 +0200

    Captcha d'inscription

---
+++ isRecaptchaValid.php
@@ -0,0 +1,34 @@
+<?php
+
+// fonction de vérification du captcha
+function isRecaptchaValid($code, $ip = null)
+{
+    if(empty($code)) {
+        return false;
+    }
+    $params = [
+        'secret'    => '6Lc9SS4UAAAAAKD1bH_Yz3kxQfv3KOrM_8MYs7oK',
+        'response'  => $code
+    ];
+    if($ip){
+        $params['remoteip'] = $ip;
+    }
+    $url = "https://www.google.com/recaptcha/api/siteverify?" . http_build_query($params);
+    if(function_exists('curl_version')){
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($curl);
+    }else{
+        $response = file_get_contents($url);
+    }
+    if(empty($response) || is_null($response)){
+        return false;
+    }
+    $json = json_decode($response);
+    return $json->success;
+}
+
+?>
